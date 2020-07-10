d1816a83bc229a4dbaded7244f5485642857368a
Brenda Profiro <brenda.santos@tripletech.com.br>
2020-07-10 16:37:15 -0300

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using apinet.Data;
+using apinet.Models;
+
+namespace apinet.Controllers
+{
+  [ApiController]
+  [Route("categories")]
+  public class CategotyController: ControllerBase
+  {
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+    {
+      var categories = await context.Categories.ToListAsync();
+      return categories;
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<Category>> Post(
+      [FromServices] DataContext context,
+      [FromBody]Category model)
+    {
+      if (ModelState.IsValid)
+      {
+        context.Categories.Add(model);
+        await context.SaveChangesAsync();
+        return model;
+      }
+      else
+      {
+        return BadRequest(ModelState);
+      }
+    }
+  }
+}
